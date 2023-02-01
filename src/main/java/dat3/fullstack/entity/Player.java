0e27b86038a51bae0d8cd414a240bc2bb4b3bd08
plaul <lmor@kea.dk>
2023-02-01 11:24:32 +0100

    with a few tests

--- src/main/java/dat3/fullstack/entity/Player.java
+++ src/main/java/dat3/fullstack/entity/Player.java
@@ -1,16 +1,14 @@
 package dat3.fullstack.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-@Entity
+@Entity(name = "spiller")
 public class Player {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column(name="spiller_navn",nullable = false)
     private String name;
 
     public Player(String name) {
--- src/main/java/dat3/fullstack/repositories/PlayerRepository.java
+++ src/main/java/dat3/fullstack/repositories/PlayerRepository.java
@@ -1,7 +1,10 @@
 package dat3.fullstack.repositories;
 
 import dat3.fullstack.entity.Player;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlayerRepository extends CrudRepository<Player,Integer> {}
+
+public interface PlayerRepository extends JpaRepository<Player,Integer> {
+    Player findPlayerByName(String name);
+}
 
